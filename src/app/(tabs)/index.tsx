2f7b287cdfd2e6ef60dca2bc7238c0c0527730d1
Krishnam Upadhyay <upadhyaykrishnam23@gmail.com>
2024-12-21 21:29:10 +0530

    bulding customer  component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,39 +1,14 @@
-import { Image, StyleSheet } from "react-native";
-
-import EditScreenInfo from "../../components/EditScreenInfo";
+import ProductListItem from "@/src/components/ProductListItem";
 import { Text, View } from "../../components/Themed";
-import Colors from "@/src/constants/Colors";
+
 import products from "@/assets/data/products";
 
 const product: any = products[0];
 
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View>
+      <ProductListItem />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 20,
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "600",
-    marginVertical: 10,
-    color: "black",
-  },
-  price: {
-    color: Colors.light.tint,
-  },
-});
